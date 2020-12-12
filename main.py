376d2b227f9704f98037aa40c3627a1c2e86b131
Nan An <38464709+AnnieAn90@users.noreply.github.com>
2020-12-11 22:04:22 -0400

    Update main.py
    updated on 11th Dec. 2020 by Nan An (Annie)

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ def read_sensor():
     temp = hum = 0
 
     try:
-        sensor.measure()
-        temp = sensor.temperature()
-        hum = sensor.humidity()
-        if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):
-            res = ('{0},{1}'.format(temp, hum))
-            return (res)
-        else:
-            return ('Invalid sensor readings.')
+      sensor.measure()
+      temp = sensor.temperature()
+      hum = sensor.humidity()
+      if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):
+        res = ('{0},{1}'.format(temp, hum))
+        return (res)
+      else:
+        return ('Invalid sensor readings.')
     except OSError as e:
         return ('Failed to read sensor.')
 
 # web_page function returns HTML page
 def web_page():
-    if temp >= 25:
-        LED.on()
-        LED_state = "ON"
-    else:
-        LED.off()
-        LED_state = "OFF"
+  if temp >= 25:
+      LED.on()
+      LED_state = "ON"
+  else:
+      LED.off()
+      LED_state = "OFF"
 
   html = """<!DOCTYPE html>
   <html>
@@ -35,42 +35,37 @@ def web_page():
       .dht-labels{font-size: 1.5rem;vertical-align:middle;padding-bottom: 15px;}
       .led-labels{font-size: 1.5rem;vertical-align:middle;padding-bottom: 15px;}
   </style>
-
   <script>
       setInterval(loadDoc,5000)
       function loadDoc() {
-           var xhttp = new XMLHttpRequest()
-           xhttp.onreadystatechange = function() {
+          var xhttp = new XMLHttpRequest()
+          xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200){ 
                     document.getElementById(\"webpage\").innerHTML =this.responseText}
-            }
-           xhttp.open(\"GET\", \"/\", true)
-           xhttp.send()
-       }
+          }
+          xhttp.open(\"GET\", \"/\", true)
+          xhttp.send()
+      }
   </script>
   </head>
-
   <body>
-
-
   <div id=\"webpage\">
       <h1>ESP8266 DHT Server</h1>
       <p>
-         <span class="dht-labels">Temperature: </span> 
-         <span>""" + str(temp) + """</span>
-         <sup class="units">&deg;C</sup>
+        <span class="dht-labels">Temperature: </span> 
+        <span>""" + str(temp) + """</span>
+        <sup class="units">&deg;C</sup>
       </p>
       <p> 
-         <span class="dht-labels">Humidity: </span>
-         <span>""" + str(hum) + """</span>
-         <sup class="units">%</sup>
+        <span class="dht-labels">Humidity: </span>
+        <span>""" + str(hum) + """</span>
+        <sup class="units">%</sup>
       </p>
       <p> 
-         <span class="led-labels">LED state: </span>
-         <span>""" + str(LED_state) + """</span>
+        <span class="led-labels">LED state: </span>
+        <span>""" + str(LED_state) + """</span>
       </p>
   </div>
-
   </body>
   </html>"""
   return html
@@ -82,22 +77,22 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # '' empty string refers to the server will accept connections on all available IPv4 interfaces.
 s.bind(('', 80))
 # It makes a listening socket with the maximum number of queued connections is 5
-s.listen(4)
+s.listen(5)
 
 # start to listen requests and send responses
 while True:
-    # accept connections
-    conn, addr = s.accept()
-    print('Got a connection from', addr)
-    # The data exchange between the client and server using the send() and recv() methods
-    # recv method specifies the maximum data that can be received at once.
-    request = conn.recv(1024)
-    request = str(request)
-    print('Content = %s' % request)
-    print(read_sensor())
-    response = web_page()
-    conn.send('HTTP/1.1 200 OK\n')
-    conn.send('Content-Type: text/html\n')
-    conn.send('Connection: close\n\n')
-    conn.sendall(response)
-    conn.close()
+  # accept connections
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  # The data exchange between the client and server using the send() and recv() methods
+  # recv method specifies the maximum data that can be received at once.
+  request = conn.recv(1024)
+  request = str(request)
+  print('Content = %s' % request)
+  print(read_sensor())
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
